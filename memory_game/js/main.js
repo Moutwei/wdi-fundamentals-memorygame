6e132606f05c6430156447735d8207adfd14e6fc
Moutwei Chap <moutwei@Moutweis-MBP.fios-router.home>
2019-09-09 03:22:50 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,19 +1,19 @@
 const cards = ["queen", "queen", "king", "king"];
 const cardsInPlay = [];
 
-var cardOne = cards[2];
-cardsInPlay.push(cardOne);
-
-console.log("User Flipped " + cardOne);
-
-var cardTwo = cards[3];
-cardsInPlay.push(cardTwo);
-
-console.log("User Flipped " + cardTwo);
-
-if(cardsInPlay.length === 2 && (cardsInPlay[0] === cardsInPlay[1])){
-	alert("You found a match!");
+function checkForMatch(){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+	console.log("You found a match!");
+	}
+	else{
+	console.log("Sorry, try again.");
+	}
 }
-else{
-	alert("Sorry, try again.")
+
+function flipCard(cardId){
+	checkForMatch();
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
 }
+
+flipCard(0);
