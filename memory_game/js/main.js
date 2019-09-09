4c9084f7c57abd78f3c7f14873851a91ab90c6a9
Moutwei Chap <moutwei@Moutweis-MBP.fios-router.home>
2019-09-09 04:00:59 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,29 @@
-const cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: 'queen',
+	suit: 'hearts',
+	cardImage: 'images/queen-of-hearts.png'
+},
+{
+	rank: 'queen',
+	suit: 'diamonds',
+	cardImage: 'images/queen-of-diamonds.png'
+},
+{
+	rank: 'king',
+	suit: 'hearts',
+	cardImage: 'images/king-of-hearts.png'
+},
+{
+	rank: 'king',
+	suit: 'diamonds',
+	cardImage: 'images/king-of-diamons.png'
+}
+];
 const cardsInPlay = [];
 
 function checkForMatch(){
-	if(cardsInPlay[0] === cardsInPlay[1]){
+	if(cardsInPlay[0] === cardsInPlay[2]){
 	console.log("You found a match!");
 	}
 	else{
@@ -12,8 +33,9 @@ function checkForMatch(){
 
 function flipCard(cardId){
 	checkForMatch();
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 }
-
 flipCard(0);
