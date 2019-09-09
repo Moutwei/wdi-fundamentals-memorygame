51b0b98d610fbddd3de5fd813d185c47c31b3a1f
Moutwei Chap <moutwei@Moutweis-MBP.fios-router.home>
2019-09-09 02:38:37 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,19 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-let cardThree = "king";
-var cardFour = "king";
-console.log("Person Flipped the 3rd card and got! " + cardThree);
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+
+var cardOne = cards[2];
+cardsInPlay.push(cardOne);
+
+console.log("User Flipped " + cardOne);
+
+var cardTwo = cards[3];
+cardsInPlay.push(cardTwo);
+
+console.log("User Flipped " + cardTwo);
+
+if(cardsInPlay.length === 2 && (cardsInPlay[0] === cardsInPlay[1])){
+	alert("You found a match!");
+}
+else{
+	alert("Sorry, try again.")
+}
