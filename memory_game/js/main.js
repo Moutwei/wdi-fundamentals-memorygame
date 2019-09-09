0af534a25e6b390ed5e1052dffd4e04c25db232c
Moutwei Chap <moutwei@Moutweis-MBP.fios-router.home>
2019-09-09 01:22:20 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,6 @@
+console.log("Up and running!");
+var cardOne = "queen";
+var cardTwo = "queen";
+let cardThree = "king";
+var cardFour = "king";
+console.log("Person Flipped the 3rd card and got! " + cardThree);
